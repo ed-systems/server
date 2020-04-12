794cf9ab8b3404c6b750deb8349c913d75aca9a0
Noah Murphy <noah.murphy@outlook.com>
2020-04-11 21:55:14 -0400

    made some changes to login

--- login.php
+++ login.php
@@ -6,6 +6,20 @@
     $un = $input_data["username"];
     $pw = sha1($input_data["password"]);
 
+
+        function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+    }
+
+    $tok=generateRandomString();
+
+
     //DB-INFO
     $servername = "sql1.njit.edu";
     $username = "npm26";
@@ -33,13 +47,28 @@
     //compare pass positive
     if($pw==$passhash){
         //print_r("SUCESSFULL login");
-        $dataArr = array("database"=> array("success"=> true, "message"=> "Successful login with $un (database)." ),"webnjit"=> array("success"=> null,"message"=> null ));
+        //$dataArr = array("database"=> array("success"=> true, "message"=> "Successful login with $un (database)." ),"webnjit"=> array("success"=> null,"message"=> null ), "");
+        $dataArr = array("token"=> $tok); 
+
+        
+        $q = $conn->query("SELECT id FROM users WHERE username='$un'");
+        $uid = $q->fetchColumn();
+
+        $updateAuthTable = 'UPDATE `auth_table` SET token=:tk WHERE id=:usid';
+        $updateTok = $conn->prepare($updateAuthTable);
+        $updateTok->bindValue(':tk', $tok);
+        $updateTok->bindValue(':usid', $uid);
+        $updateTok->execute();
+
+
+
     }
 
     //compare pass negative
     if($pw!=$passhash){
         //print_r("UNSUCESSFULL login");
-        $dataArr = array("database"=> array("success"=> false, "message"=> "Unsuccessful login with $un (database)." ),"webnjit"=> array("success"=> null,"message"=> null ));
+        //$dataArr = array("database"=> array("success"=> false, "message"=> "Unsuccessful login with $un (database)." ),"webnjit"=> array("success"=> null,"message"=> null ));
+        $dataArr = array("token"=> $tok); 
     }
     
     //close db connection
