340ce91326726276e21bb8c2d9ee6f1a44862b80
Noah Murphy <noah.murphy@outlook.com>
2020-04-11 20:57:15 -0400

    Fixed bug

--- save_student_submission.php
+++ save_student_submission.php
@@ -46,16 +46,13 @@ function querry_middle($in)
 
 function save_student_submissions($json, $conn)
 {
-  //update status = 1
   $update_status_query = 'UPDATE `submissions` SET status = 1 WHERE id = :id';
   $update_status = $conn->prepare($update_status_query);
   $update_status->bindValue(':id', $json['ID']);
   $update_status->execute();
-  //for each question...
 
   $autograde_sum = 0;
-  //echo $json['ID'];
-  //subid=$json['ID'];
+
 
 
 
@@ -66,10 +63,7 @@ function save_student_submissions($json, $conn)
   $update_gradecomments_query = 'UPDATE `submissions` SET autograde=:autoG WHERE id=:subID';
 
   $update_gradecomments = $conn->prepare($update_gradecomments_query);
-
-  //get grade from autograder
   $G = $json['autoGrade'];
-  //$G=99;
   $update_gradecomments->bindValue(':autoG', $G);
 
   $sid = $json['ID'];
@@ -85,16 +79,10 @@ function save_student_submissions($json, $conn)
   $tok = $q->fetchColumn();
 
 
-  //token
-
-
-
   $questionsArr = array("solution" => "", "ID" => "");
   $arrQarr = array();
 
   foreach ($json['questions'] as $question) {
-
-
     $update_questioncomments_query = 'UPDATE `submitted_questions` SET solution=:sol, result1=:res1, result2=:res2, autograde=:ag WHERE subID=:sd AND questionID=:qd';
 
     $qid = $question['ID'];
@@ -102,28 +90,45 @@ function save_student_submissions($json, $conn)
     $update_questioncomments = $conn->prepare($update_questioncomments_query);
 
     $S = $question['solution'];
-    //echo json_encode($S);
+
+
+
+
     $update_questioncomments->bindValue(':sol', $S);
-
-    //get grade from autoograde
-    // $G=$question['autoGrade'];
-    //$G=66;
-    //$update_questioncomments->bindValue(':ag', $G);
-
 
     $update_questioncomments->bindValue(':sd', $sid);
 
     $update_questioncomments->bindValue(':qd', $question['ID']);
 
 
-    //$update_questioncomments->execute();
 
 
-    //return string
     $questionsArr["solution"] = $question['solution'];
+
     $questionsArr["ID"] = $question['ID'];
 
-    $user_exams_obj = array("input1" => $question['input1'], "input2" => $question['input2'], "output1" => $question['output1'], "output2" => $question['output2'], "points" => $question['points'], "questionID" => $question['questionID'], "solution" => $question['solution']);
+
+
+    
+    $q = $conn->query("SELECT points FROM exam_questions WHERE questionID= '$qid'");
+    $pts = $q->fetchColumn();
+
+    $q = $conn->query("SELECT input1 FROM questions WHERE id= '$qid'");
+    $i1 = $q->fetchColumn();
+    $q = $conn->query("SELECT input2 FROM questions WHERE id= '$qid'");
+    $i2 = $q->fetchColumn();
+    
+    $q = $conn->query("SELECT output1 FROM questions WHERE id= '$qid'");
+    $o1 = $q->fetchColumn();
+    $q = $conn->query("SELECT output2 FROM questions WHERE id= '$qid'");
+    $o2 = $q->fetchColumn();
+
+    //$user_exams_obj = array("input1" => $question['input1'], "input2" => $question['input2'], "output1" => $question['output1'], "output2" => $question['output2'], "points" => $question['points'], "questionID" => $question['questionID'], "solution" => $question['solution']);
+    $user_exams_obj = array("questionID" => $qid, "points" => $pts, "solution" => $S, "input1" => $i1,"output1" => $o1, "input2" => $i2, "output2" => $o2);
+    echo $user_exams_obj;
+
+
+
 
     $resultspackage = querry_middle($user_exams_obj);
 
@@ -132,14 +137,15 @@ function save_student_submissions($json, $conn)
 
 
 
-
-
-    //echo $resultspackage[0];
-    // echo $resultspackage[1];
     $r1 = $results['result1'];
+
     $r2 = $results['result2'];
+
     $update_questioncomments->bindValue(':res1', $r1);
+
     $update_questioncomments->bindValue(':res2', $r2);
+
+
 
 
     $g = $results['autoGrade'];
@@ -148,60 +154,16 @@ function save_student_submissions($json, $conn)
     $update_questioncomments->execute();
 
     array_push($arrQarr, $questionsArr);
-  }  //echo json_encode($user_exams_obj);
-
-
-  //querry_middle($user_exams_obj);
-
-
-  //$output_data = json_encode($user_exams_obj);
-
-  //json header
-  // header('Content-Type: application/json');
-
-
-  // echo $output_data;  
-
-
-
-
-  //foreach($json['questions'] as $question){
-
-
-
-  // }
-
-
-
+  }
 }
 
 
 
 try {
-
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  //echo "first";
-
   save_student_submissions($json, $conn);
-
-  //echo json_encode($json['ID']);
-
-  //$output_data = json_encode($saved_subs);
-
-  //json header
-  // header('Content-Type: application/json');
-
-
-  //echo $output_data;  
-
-
-}
-
-//err handling
-catch (PDOException $e) {
+} catch (PDOException $e) {
   echo $sql . "<br>" . $e->getMessage();
 }
 
