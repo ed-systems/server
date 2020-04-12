cf78b75a34148d4ff33626781eb73b6c39079854
Noah Murphy <noah.murphy@outlook.com>
2020-04-12 17:29:13 -0400

    msc

--- save_student_submission.php
+++ save_student_submission.php
@@ -169,7 +169,7 @@ function save_student_submissions($json, $conn)
     //$user_exams_obj = array("input1" => $question['input1'], "input2" => $question['input2'], "output1" => $question['output1'], "output2" => $question['output2'], "points" => $question['points'], "questionID" => $question['questionID'], "solution" => $question['solution']);
     $user_exams_obj = array("questionID" => $qid, "points" => $pts, "solution" => $S, "input1" => $i1, "input2" => $i2, "input3" => $i3, "input4" => $i4, "input5" => $i5, "input6" => $i6, "output1" => $o1, "output2" => $o2, "output3" => $o3, "output4" => $o4, "output5" => $o5, "output6" => $o6, "output1_points" => $op1, "output2_points" => $op2, "output3_points" => $op3, "output_points4" => $op4, "output_points5" => $op5, "output6_points" => $op6);
     //also send input/output points, function name&points, constraint & points, colon_points,
-    //echo json_encode($user_exams_obj);
+    echo json_encode($user_exams_obj);
 
 
 
