11b86ddceb113a48517c4f038e5c9a2947daee41
Noah Murphy <noah.murphy@outlook.com>
2020-04-11 23:59:41 -0400

    msc

--- get_user_exams.php
+++ get_user_exams.php
@@ -91,7 +91,11 @@ function gue($json, $conn){
         $subminfo->execute();
         $minfo = $subminfo->fetchAll(\PDO::FETCH_ASSOC);
 
-        
+        foreach($minfo as $info){
+
+          echo $info['name'];
+
+        }
 
 
 
@@ -102,7 +106,7 @@ function gue($json, $conn){
 
       array_push($user_exams, $user_exams_obj);
     }
-     echo $minfo['name'];
+     //echo $minfo['name'];
      echo json_encode($en);
   }
  
