7df137d1be68432c98b5824e52fe05a2a07faf01
Noah Murphy <noah.murphy@outlook.com>
2020-04-12 17:01:00 -0400

    msc

--- save_student_submission.php
+++ save_student_submission.php
@@ -81,7 +81,7 @@ function save_student_submissions($json, $conn)
 
   $questionsArr = array("solution" => "", "ID" => "");
   $arrQarr = array();
- echo "here1";
+ //echo "here1";
   //exam score counter
   $eMarks = 0;
 
@@ -119,16 +119,52 @@ function save_student_submissions($json, $conn)
     $i1 = $q->fetchColumn();
     $q = $conn->query("SELECT input2 FROM questions WHERE id= '$qid'");
     $i2 = $q->fetchColumn();
+    $q = $conn->query("SELECT input3 FROM questions WHERE id= '$qid'");
+    $i2 = $q->fetchColumn();
+    $q = $conn->query("SELECT input4 FROM questions WHERE id= '$qid'");
+    $i4 = $q->fetchColumn();
+    $q = $conn->query("SELECT input5 FROM questions WHERE id= '$qid'");
+    $i5 = $q->fetchColumn();
+    $q = $conn->query("SELECT input6 FROM questions WHERE id= '$qid'");
+    $i6 = $q->fetchColumn();
+    
     
     $q = $conn->query("SELECT output1 FROM questions WHERE id= '$qid'");
     $o1 = $q->fetchColumn();
     $q = $conn->query("SELECT output2 FROM questions WHERE id= '$qid'");
     $o2 = $q->fetchColumn();
+    $q = $conn->query("SELECT output3 FROM questions WHERE id= '$qid'");
+    $o3 = $q->fetchColumn();
+    $q = $conn->query("SELECT output4 FROM questions WHERE id= '$qid'");
+    $o4 = $q->fetchColumn();
+    $q = $conn->query("SELECT output5 FROM questions WHERE id= '$qid'");
+    $o5 = $q->fetchColumn();
+    $q = $conn->query("SELECT output6 FROM questions WHERE id= '$qid'");
+    $o6 = $q->fetchColumn();
 
+
+    $q = $conn->query("SELECT output_points1 FROM exam_questions WHERE questionID= '$qid' AND examID=$eid");
+    $op1 = $q->fetchColumn();
+    $q = $conn->query("SELECT output_points2 FROM exam_questions WHERE questionID= '$qid' AND examID=$eid");
+    $op2 = $q->fetchColumn();
+    $q = $conn->query("SELECT output_points3 FROM exam_questions WHERE questionID= '$qid' AND examID=$eid");
+    $op3 = $q->fetchColumn();
+    $q = $conn->query("SELECT output_points4 FROM exam_questions WHERE questionID= '$qid' AND examID=$eid");
+    $op4 = $q->fetchColumn();
+    $q = $conn->query("SELECT output_points5 FROM exam_questions WHERE questionID= '$qid' AND examID=$eid");
+    $op5 = $q->fetchColumn();
+    $q = $conn->query("SELECT output_points6 FROM exam_questions WHERE questionID= '$qid' AND examID=$eid");
+    $op6 = $q->fetchColumn();
+
+    
+
+
+
+    
     //$user_exams_obj = array("input1" => $question['input1'], "input2" => $question['input2'], "output1" => $question['output1'], "output2" => $question['output2'], "points" => $question['points'], "questionID" => $question['questionID'], "solution" => $question['solution']);
-    $user_exams_obj = array("questionID" => $qid, "points" => $pts, "solution" => $S, "input1" => $i1,"output1" => $o1, "input2" => $i2, "output2" => $o2);
+    $user_exams_obj = array("questionID" => $qid, "points" => $pts, "solution" => $S, "input1" => $i1, "input2" => $i2, "input3" => $i3, "input4" => $i4, "input5" => $i5, "input6" => $i6, "output1" => $o1, "output2" => $o2, "output3" => $o3, "output4" => $o4, "output5" => $o5, "output6" => $o6, "output1_points" => $op1, "output2_points" => $op2, "output3_points" => $op3, "output_points4" => $op4, "output_points5" => $op5, "output6_points" => $op6);
     //also send input/output points, function name&points, constraint & points, colon_points,
-    //echo json_encode($user_exams_obj);
+    echo json_encode($user_exams_obj);
 
 
 
@@ -174,7 +210,7 @@ function save_student_submissions($json, $conn)
   $update_gradecomments->bindValue(':subID', $sid);
 
   $update_gradecomments->execute();
-echo "here2"
+//echo "here2"
   // update exam submission record which includes the sum of the scores of the each question
 }
 
