0ed0b4290e294fe176246ef6146571cb074b9295
Noah Murphy <noah.murphy@outlook.com>
2020-04-13 14:05:38 -0400

    msc

--- save_exam.php
+++ save_exam.php
@@ -106,7 +106,7 @@
 
 
                     $sql = "INSERT INTO exam_questions (examID, questionID, points, output1_points, output2_points, output3_points, output4_points, output5_points, output6_points, functionNamePoints, constraintStringPoints, colonPoints)
-                    VALUES ('$eid', '$qid', 99, '$op1', '$op2', '$op3', '$op4', '$op5', '$op6', '$fnp', '$csp', '$cp')";
+                    VALUES ('$eid', '$qid', 66, '$op1', '$op2', '$op3', '$op4', '$op5', '$op6', '$fnp', '$csp', '$cp')";
                     $conn->query($sql);
                 }
                 $message = "New exam record created successfully";
@@ -171,7 +171,7 @@
 
 
                     $sql = "INSERT INTO exam_questions (examID, questionID, points, output1_points, output2_points, output3_points, output4_points, output5_points, output6_points, functionNamePoints, constraintStringPoints, colonPoints)
-                    VALUES ('$eid', '$qid', 99, '$op1', '$op2', '$op3', '$op4', '$op5', '$op6', '$fnp', '$csp', '$cp')";
+                    VALUES ('$eid', '$qid', 77, '$op1', '$op2', '$op3', '$op4', '$op5', '$op6', '$fnp', '$csp', '$cp')";
                     $conn->query($sql);
                 }
 
