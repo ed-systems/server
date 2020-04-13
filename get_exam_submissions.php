18f6a9f5bbd466514981c0842a1355df92c36c3b
Noah Murphy <noah.murphy@outlook.com>
2020-04-13 16:06:55 -0400

    msc

--- get_exam_submissions.php
+++ get_exam_submissions.php
@@ -97,12 +97,12 @@
               $questionsArr["constraint_result"]=$q['constraint_result'];
               $questionsArr["constraint_result_points"]=$q['constraint_result_points'];
 
-              $questionsArr["output1_result"]=$q['output1_points'];
-              $questionsArr["output2_result"]=$q['output2_points'];
-              $questionsArr["output3_result"]=$q['output3_points'];
-              $questionsArr["output4_result"]=$q['output4_points'];
-              $questionsArr["output5_result"]=$q['output5_points'];
-              $questionsArr["output6_result"]=$q['output6_points'];
+              $questionsArr["output1_points"]=$q['output1_points'];
+              $questionsArr["output2_points"]=$q['output2_points'];
+              $questionsArr["output3_points"]=$q['output3_points'];
+              $questionsArr["output4_points"]=$q['output4_points'];
+              $questionsArr["output5_points"]=$q['output5_points'];
+              $questionsArr["output6_points"]=$q['output6_points'];
               
               array_push($arrQarr, $questionsArr);
 
