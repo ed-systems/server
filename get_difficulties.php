80efbe513c0070f8a680d80b5154192fb3a10232
Noah Murphy <noah.murphy@outlook.com>
2020-04-12 20:45:44 -0400

    created get_difficulties

---
+++ get_difficulties.php
@@ -0,0 +1,59 @@
+<?php
+
+
+// pass json_decode'd data into these 
+$json = json_decode(file_get_contents('php://input'), true);
+
+
+//get difficulties
+function getD(){
+
+
+    $difficultyInfoQuery = 'SELECT * FROM difficulties';
+    $difficultyInfo = $conn->prepare($difficultyInfoQuery);
+    $difficultyInfo->execute();
+    $dinfo = $difficultyInfo->fetchAll(\PDO::FETCH_ASSOC);
+    
+
+    $difficulty_array=array();
+
+
+        foreach($dinfo as $info){
+     
+        
+            $dId = $info['id'];
+            $dName = $info['difficulty_string'];
+            $dDescription = $info['difficulty_description'];
+
+            $difficulty_array_obj=array("id" => $dId, "name" => $dName, "description" => $dDescription);
+            array_push($difficulty_array_obj, $difficulty_array);
+
+
+        }
+
+
+    return json_encode($difficulty_array);
+
+
+}
+
+
+$servername = "sql1.njit.edu";
+$username = "npm26";
+$password = "DBPassword1!";
+$dbname = "npm26";
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    getD();
+  } catch (PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+  
+
+$conn = null;
+
+
+?>
