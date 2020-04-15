f111c0ff75fd4266bed9db27c704e9ffce419a23
Noah Murphy <noah.murphy@outlook.com>
2020-04-15 04:01:09 -0400

    msc

--- get_exams.php
+++ get_exams.php
@@ -136,8 +136,13 @@
              // $q = $conn->query("SELECT examName FROM exam_questions_with_userid WHERE examID = '$id'");
               $n = $q->fetchColumn();
 
-              $q = $conn->query("SELECT examDescription FROM exam_questions_with_userid WHERE `examName` = '$n'");
-              $d = $q->fetchColumn();
+
+              $pps=$conn->prepare("SELECT `examDescription` FROM `exam_questions_with_userid` WHERE `examName` =:n")
+              $pps->bindParam(':n', $n);
+              $pps->execute();
+
+              //$q = $conn->query("SELECT examDescription FROM exam_questions_with_userid WHERE `examName` = '$n'");
+              //$d = $q->fetchColumn();
               //echo $d;
               //exam info obj
               $exam_obj = array("name" => $n, "description"=> $d, "questions" => $arrQarr, "id" => $id);
