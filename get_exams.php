dbeb228c8a5daf9f5f2ba56558f2781dd2144b1b
Noah Murphy <noah.murphy@outlook.com>
2020-04-15 04:02:23 -0400

    msc

--- get_exams.php
+++ get_exams.php
@@ -137,7 +137,7 @@
               $n = $q->fetchColumn();
 
 
-              $pps=$conn->prepare("SELECT `examDescription` FROM `exam_questions_with_userid` WHERE `examName` =:n")
+              $pps = $conn->prepare("SELECT `examDescription` FROM `exam_questions_with_userid` WHERE `examName` =:n");
               $pps->bindParam(':n', $n);
               $pps->execute();
 
